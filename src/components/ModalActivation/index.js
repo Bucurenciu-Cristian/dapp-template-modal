f3fd896c4b440e716146ef0929d7d10c73405c16
Bucurenciu Bogdan Cristian <bucurenciu.cristian@gmail.com>
2022-10-05 17:50:54 +0300

    small fix

--- src/components/ModalActivation/index.js
+++ src/components/ModalActivation/index.js
@@ -1,7 +1,4 @@
-import React, {
-  useEffect,
-  useRef
-} from 'react';
+import React, { useEffect, useRef } from 'react';
 import { createPortal } from 'react-dom';
 
 const ModalActivation = ({ children }) => {
